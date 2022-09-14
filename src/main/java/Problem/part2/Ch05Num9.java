3c06335488bbc133ccf5c67d443792eb9ef7baec
Anyeon00 <seluo65@naver.com>
2022-09-14 23:02:00 +0900

    9/13

--- src/main/java/Problem/part2/Ch05Num9.java
+++ src/main/java/Problem/part2/Ch05Num9.java
@@ -1,34 +1,36 @@
 package Problem.part2;
 
+import java.util.Scanner;
+
 public class Ch05Num9 {
     public static void main(String[] args) {
-/*        Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
 
         while (true) {
             System.out.println("--------------------------------------------------");
             System.out.println("1.학생수 | 2.점수입력 | 3.점수리스트 | 4.분석 | 5.종료");
             System.out.println("--------------------------------------------------");
             System.out.print("선택> ");
-            String choose = sc.nextLine();
 
-            int arr[];
+            int[] arr = new int[100];
             int sum = 0;
             int max = 0;
 
-            if (choose == "1") {
+            int choose = sc.nextInt();
+            if (choose == 1) {
                 System.out.print("학생수> ");
                 int a = sc.nextInt();
                 arr = new int[a];
-            } else if (choose == "2") {
+            } else if (choose == 2) {
                 for(int i = 0; i < arr.length; i++){
                     System.out.print("scores[" + i + "]> ");
                     arr[i] = sc.nextInt();
                 }
-            } else if (choose == "3"){
+            } else if (choose == 3){
                 for(int i = 0; i < arr.length; i++) {
-                    System.out.print("scores[" + i + "]: " + arr[i]);
+                    System.out.println("scores[" + i + "]: " + arr[i]);
                 }
-            } else if (choose == "4"){
+            } else if (choose == 4){
                 for (int i = 0; i < arr.length; i++) {
                     if (arr[i] > max) {     //max
                         max = arr[i];
@@ -38,10 +40,10 @@ public class Ch05Num9 {
                 double avg = (double) sum / arr.length;
                 System.out.println("최고 점수: " + max);
                 System.out.println("평균 점수: " + avg);
-            } else {
+            } else if(choose == 5){
                 System.out.println("프로그램 종료");
                 break;
             }
-        }*/
+        }
     }
 }
